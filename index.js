842c24f548dd19813e324fa89852970cc398b4dc
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2023-08-11 15:13:53 +0300

    middleware

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use(function(req,res){           //gelen her sorguda 'hello world yazisi getirilir
-    res.end("hello world")
+app.use(function(req,res,next){           //gelen her sorguda 'hello world yazisi getirilir
+    console.log("middleware 1")
+    next();  //süreci devam ettir
+});
+
+app.use(function(req,res,next){          
+    console.log("middleware 2") 
+    next();
+});
+
+app.use(function(req,res){          
+    console.log("middleware 3") 
+    //res.end();   //bitir
+    res.send("<h1>homepage</h1>")
 });
 
 app.listen(3000, function(){
