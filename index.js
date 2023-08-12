0aca3a7419459847b142f8b76288a2b5a5be5cf8
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2023-08-12 16:14:02 +0300

    add express router

--- index.js
+++ index.js
@@ -3,29 +3,14 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+const userRoutes = require("./routes/user");   //middleware
+const adminRoutes = require("./routes/admin");
 
 app.use("/libs",express.static(path.join(__dirname,'node_modules')));
 app.use("/static",express.static(path.join(__dirname,'public')));
 
-app.use("/blogs/:blogid",function(req,res){   
-     //res.sendFile("/views/users/details.html")
-    console.log(__dirname)         //dirname mevcut klasörü gösterir
-    console.log(__filename)        //dirname mevcut dosyayı gösterir
-
-    //C://blogapp/views/user/details.htmlolmasi icin
-    res.sendFile(path.join(__dirname, "views/users","blog-details.html"))
-   
-});
-
-app.use("/blogs",function(req,res){    
-    res.sendFile(path.join(__dirname, "views/users","blogs.html"))
-
-});
-
-app.use("/",function(req,res){    
-    res.sendFile(path.join(__dirname, "views/users","index.html"))
-
-});
+app.use("admin",adminRoutes);   //daha özel daha yukarıda
+app.use(userRoutes);
 
 app.listen(3000, function(){
     console.log("Listening on port 3000")
