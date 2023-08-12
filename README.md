f2f8b95f4aa71b9bee835712e5c27ec13180a9e9
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2023-08-12 16:51:09 +0300

    add ejs

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ npm i nodemon@2.0.19 --save-dev //gelistirme de yardimci
 
 npm i bootstrap@5.2.0
 
-## RUN
+npm i ejs@3.1.8
+
+## RUNSS
 
 n̶o̶d̶e̶ ̶i̶n̶d̶e̶x̶.̶j̶s̶
 
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 const path = require("path");
 const userRoutes = require("./routes/user");   //middleware
 const adminRoutes = require("./routes/admin");
@@ -9,7 +11,7 @@ const adminRoutes = require("./routes/admin");
 app.use("/libs",express.static(path.join(__dirname,'node_modules')));
 app.use("/static",express.static(path.join(__dirname,'public')));
 
-app.use("admin",adminRoutes);   //daha özel daha yukarıda
+app.use("/admin",adminRoutes);   //daha özel daha yukarıda
 app.use(userRoutes);
 
 app.listen(3000, function(){
