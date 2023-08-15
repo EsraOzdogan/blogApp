454d689bc701477ce8f2a2f5dd80efcc08c00540
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2023-08-15 14:18:50 +0300

    add blog list with foreach

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,31 @@ const router = express.Router();
 
 const data = {
    title: "Popular Courses",
-   categories:["Web Development","Mobile Application","Data Analysis","Programming","Office Applications"]
+   categories:["Web Development","Mobile Application","Data Analysis","Programming","Office Applications"],
+   blogs: [
+      {
+         blogId: 1,
+         title: "Applied Web development",
+         description : "'Web Development': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+         img: "1.jpg",
+         homepage: true,
+      },
+       {
+         blogId: 2,
+         title: "Python Programming",
+         description : "Python Tutorials. Database, Data Analysis, Bot Writing, Web Development(Django)",
+         img: "2.jpg",
+         homepage: true,
+
+      },
+       {
+         blogId: 3,
+         title: "Javascript Programming",
+         description : "Nodejs, Angular, React and VueJs with modern javascript tutorials (ES6 & ES7+).",
+         img: "3.jpg",
+         homepage: false,
+      },
+   ]
 };
 
 router.use("/blogs/:blogid",function(req,res){   
@@ -11,7 +35,7 @@ router.use("/blogs/:blogid",function(req,res){
 });
 
 router.use("/blogs",function(req,res){    
-   res.render( "users/blogs");
+   res.render( "users/blogs", data);
 });
 
 router.use("/",function(req,res){    
