3744092fbd3dbde4d4c1e52bfcd5304b6338efdf
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2023-08-21 18:27:09 +0300

    get data from category table to select option

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/blog/create",function(req,res){   
-   res.render( "admin/blog-create", { title: 'add blog'});
+const db = require("../data/db");
 
-   
+router.use("/blog/create",async function(req,res){   
 
+   try{
+      const [categories, ] = await db.execute("select * from category");
+
+      res.render( "admin/blog-create", { 
+         title: 'add blog',
+         categories: categories
+      });
+   }catch(err){
+      console.log(err)
+   }
 });
 
 router.use("/blogs/:blogId",function(req,res){    
