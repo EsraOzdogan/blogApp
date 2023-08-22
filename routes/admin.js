980ecaec90516e0713fb3a7d2738150238fe86f3
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2023-08-22 12:03:40 +0300

    add blog with post

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const db = require("../data/db");
 
-router.use("/blog/create",async function(req,res){   
-
+router.get("/blog/create",async function(req,res){   
+   
    try{
       const [categories, ] = await db.execute("select * from category");
 
@@ -17,11 +17,31 @@ router.use("/blog/create",async function(req,res){
    }
 });
 
-router.use("/blogs/:blogId",function(req,res){    
+router.post("/blog/create",async function(req,res){   
+   console.log(req.body)  //formda girilen data bilgilerini verir        //req. ejsteki htmldeki name'ler
+   const title = req.body.title;
+   const description = req.body.description;
+   const img = req.body.img;
+   const category = req.body.category;
+   const isHomepage = req.body.homepage == "on" ? 1:0;
+   const isApproved = req.body.approval == "on" ? 1:0;
+
+   try{
+      await db.execute("INSERT INTO blog(title, description, img, isHomepage, isApproved, categoryId) VALUES (?,?,?,?,?,?)",          //db veri ekleme //sıra önemli ve dbdeki columnlar ile aynı olmali
+      [title, description, img, isHomepage, isApproved, category]);
+      res.redirect("/")
+   }catch(err){
+      console.log(err);
+   }
+   
+
+});
+
+router.get("/blogs/:blogId",function(req,res){    
     res.render( "admin/blog-edit");
  });
 
-router.use("/blogs",function(req,res){    
+router.get("/blogs",function(req,res){    
    res.render( "admin/blog-list");
 });
 
